c4608747dcb19e8e24d7293064a5b44fb46afffd
Ram Krishna Yadav <rkpy4234@gmail.com>
2025-01-09 14:28:49 +0545

    new

--- Auth/authApp/urls.py
+++ Auth/authApp/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("",views.home,name="home"),
     path('login/', views.login_page, name="login"),
+    path('users/', views.users_page, name="users"),
+    path('register/', views.register_page, name="register"),
 ]
--- Auth/authApp/views.py
+++ Auth/authApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def home(request):
@@ -6,5 +8,23 @@ def home(request):
     return render(request, "authApp/home.html",context)
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "").strip()
+        password = request.POST.get("password", "")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('../users/')
+        else:
+            return render(request, 'authApp/login.html', {'error': 'Invalid username or password'})
+
     return render(request, "authApp/login.html")
+
+#for register page
+def register_page(request):
+    return render(request, "authApp/register.html")
+
+#for users page
+def users_page(request):
+    return render(request, "authApp/users.html")
 
